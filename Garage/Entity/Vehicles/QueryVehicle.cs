7b58749015a55bda5444318930fba42d2c65bebe
Yogaledare <marcus_uppstrom@hotmail.com>
2024-03-27 09:02:35 +0100

    QueryVehicle.cs

---
+++ Garage/Entity/Vehicles/QueryVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Garage.Entity.Vehicles;
+
+public class QueryVehicle : Vehicle {
+    public override string ToString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.Append(LicencePlate is not null ? $"LicencePlate={LicencePlate} " : "");
+        stringBuilder.Append(NumWheels is not null ? $"NumWheels={NumWheels} " : "");
+        stringBuilder.Append(Color is not null ? $"Color={Color} " : "");
+        stringBuilder.Append(TopSpeed is not null ? $"TopSpeed={TopSpeed} " : "");
+        var output = stringBuilder.ToString().Trim();
+        return output != "" ? output : "(Empty)";
+    }
+}
